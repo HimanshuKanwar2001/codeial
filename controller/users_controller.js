97c8966da665d3c331fe51f88d9771233c5eb8ac
unknown <himanshukanwar2001@gmail.com>
2023-12-01 00:36:09 +0530

    sign in and sign up forms created

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -18,3 +18,13 @@ module.exports.signIn=function(req,res){
     title:"Codeial | Sign Up"
   })
 }
+
+//get the sign up data
+module.exports.create=function (req,res){
+  //TODO later
+}
+
+//sign in and create a session for the user
+module.exports.createSession=function(req,res){
+  //TODO later
+}
