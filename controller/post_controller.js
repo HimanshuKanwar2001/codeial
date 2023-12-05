301792568e669c936837bb10b9273e5236da5efd
unknown <himanshukanwar2001@gmail.com>
2023-12-05 22:46:59 +0530

    saving post to the db

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,3 +1,16 @@
-module.exports.postingData=function(req,res){
-    res.end('<h1>POST Controller </h1>')
+const Post=require('../models/post')
+
+
+module.exports.create=async function(req,res){
+    try{
+            const user = await Post.create({
+              content: req.body.content,
+              user: req.user._id,
+            });
+            console.log(user);
+            return res.redirect('back');
+    }catch(err){
+        console.err("Error : ",err.message)
+        return res.status(500).send("Internal Server Error");
+    }
 }
