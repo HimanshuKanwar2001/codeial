33879aea711377d866734fac12ba8369d1b5bbdd
unknown <himanshukanwar2001@gmail.com>
2023-12-18 16:12:48 +0530

    converting to AJA and added file upload in it and now starting with APIs(making PIs and JWT authemtication)

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const fs = require("fs");
+const path = require("path");
 
 module.exports.profile = async function (req, res) {
   try {
@@ -14,16 +16,46 @@ module.exports.profile = async function (req, res) {
 };
 
 module.exports.update = async function (req, res) {
-  try {
-    if (req.user.id == req.params.id) {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body);
+  //   try {
+  //     if (req.user.id == req.params.id) {
+  //       const user = await User.findByIdAndUpdate(req.params.id, req.body);
+  //       return res.redirect("back");
+  //     } else {
+  //       return res.status(401).send("Unauthorized");
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //     return res.status(500).send("Internal Server Error");
+  //   }
+  // };
+  if (req.user.id == req.params.id) {
+    try {
+      let user = await User.findById(req.params.id);
+
+      await User.uploadedAvatar(req, res, async function (err) {
+        if (err) {
+          console.log("*** Multer Eroor:", err);
+        }
+
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if (req.file) {
+          if (user.avatar) {
+            fs.unlinkSync(path.join(__dirname, "..", user.avatar));
+          }
+
+          // this is saving the path of the uploaded file into the avatar field in the user
+          user.avatar = User.avatarPath + "/" + req.file.filename;
+          console.log("user.avatar:", user.avatar);
+        }
+        await user.save();
+        return res.redirect("back");
+      });
+    } catch (err) {
+      req.flash("error", err);
       return res.redirect("back");
-    } else {
-      return res.status(401).send("Unauthorized");
     }
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Internal Server Error");
   }
 };
 
--- index.js
+++ index.js
@@ -4,29 +4,35 @@ const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mogoose");
+const path = require("path");
 //used for session cookie
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 const MongoStore = require("connect-mongo")(session);
-const sassMiddleware=require('node-sass-middleware');
-const flash=require('connect-flash');
-const customMware=require('./config/middleware');
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
-
-app.use(sassMiddleware({
-  src:'./assets/scss',
-  dest:'./assets/css',
-  debug:true,
-  outputStyle:'extended',
-  prefix:'/css'
-}))
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
 app.use(express.static("./assets"));
+
+//make the uploads path available to the browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
 //extract style and scripts from sub pages into the layout
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
